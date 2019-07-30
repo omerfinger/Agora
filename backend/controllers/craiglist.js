ba36a681022bf6a463fd368ca0fb06deae02de04
MatanD <matan@outlook.com>
2019-07-30 20:14:42 +0300

    No images from craigslist

--- backend/controllers/craiglist.js
+++ backend/controllers/craiglist.js
@@ -6,22 +6,18 @@ let client = new craigslist.Client({
   city : 'newyork'
 });
 
-router.get('/', async (req, res) => {
-  let freeItems = await client.search({maxPrice: '0', hasPic: true}, 'free');
-  let shortItems = [];
-  for (i = 0; i < 12; i++) {
-    let x = getRndInteger(0, freeItems.length - 1);
-    if (!freeItems[x]) continue;
-
-    let detailedItem = await client.details(freeItems[x]);
-    if (!detailedItem.images) continue;
-    shortItems.push({
-      title: detailedItem.title,
-      image: detailedItem.images[0],
-      url: detailedItem.url
-    });
-  }
-  res.json(shortItems.slice(0, 5));
+router.get('/', (req, res) => {
+  client.search({maxPrice: '0'}, 'free').then((freeItems) => {
+    let shortItems = [];
+    for (let i = 0; i < 10; i++) {
+      let x = getRndInteger(0, freeItems.length - 1);
+      shortItems.push({
+        title: freeItems[x].title,
+        url: freeItems[x].url
+      });
+    }
+    res.json(shortItems);
+  });
 });
 
 function getRndInteger(min, max) {
