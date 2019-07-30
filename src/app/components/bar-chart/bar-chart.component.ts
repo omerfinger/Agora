e7eace37fcc1f1265f8a82fa2060599f78c1d6af
omerfinger <omerfinger@gmail.com>
2019-07-30 21:26:55 +0300

    canvas and other css tricks

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, OnChanges } from '@angular/core';
-import { barData } from '../../models/bar-data';
-import * as d3 from 'd3';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  OnChanges
+} from "@angular/core";
+import { barData } from "../../models/bar-data";
+import * as d3 from "d3";
 
 @Component({
-  selector: 'bar-chart',
-  templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.css'
-]
+  selector: "bar-chart",
+  templateUrl: "./bar-chart.component.html",
+  styleUrls: ["./bar-chart.component.css"]
 })
 export class BarChartComponent implements OnInit, OnChanges {
-  @ViewChild('barChart')
+  @ViewChild("barChart")
   private chartContainer: ElementRef;
 
   @Input()
@@ -17,28 +23,37 @@ export class BarChartComponent implements OnInit, OnChanges {
 
   margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   ngOnChanges(): void {
-    if (!this.data) { return; }
+    if (!this.data) {
+      return;
+    }
 
     this.createChart();
   }
 
   private createChart(): void {
     const element = this.chartContainer.nativeElement;
+
     const data = this.data;
 
-    d3.select(element).select('svg').remove();
+    d3.select(element)
+      .select("svg")
+      .remove();
 
-    const svg = d3.select(element).append('svg')
-      .attr('width', element.offsetWidth)
-      .attr('height', element.offsetHeight);
+    const svg = d3
+      .select(element)
+      .append("svg")
+      .attr("width", element.offsetWidth)
+      .attr("height", element.offsetHeight);
 
-    const contentWidth = element.offsetWidth - this.margin.left - this.margin.right;
-    const contentHeight = element.offsetHeight - this.margin.top - this.margin.bottom;
+    const contentWidth =
+      element.offsetWidth - this.margin.left - this.margin.right;
+    const contentHeight =
+      element.offsetHeight - this.margin.top - this.margin.bottom;
 
     const x = d3
       .scaleBand()
@@ -51,32 +66,37 @@ export class BarChartComponent implements OnInit, OnChanges {
       .rangeRound([contentHeight, 0])
       .domain([0, d3.max(data, d => d.frequency)]);
 
-    const g = svg.append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+    const g = svg
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")"
+      );
 
-    g.append('g')
-      .attr('class', 'axis axis--x')
-      .attr('transform', 'translate(0,' + contentHeight + ')')
+    g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + contentHeight + ")")
       .call(d3.axisBottom(x));
 
-    g.append('g')
-      .attr('class', 'axis axis--y')
+    g.append("g")
+      .attr("class", "axis axis--y")
       .call(d3.axisLeft(y))
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '0.71em')
-      .attr('text-anchor', 'end')
-      .text('Frequency');
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Frequency");
 
-    g.selectAll('.bar')
+    g.selectAll(".bar")
       .data(data)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => x(d.letter))
-      .attr('y', d => y(d.frequency))
-      .attr('width', x.bandwidth())
-      .attr('height', d => contentHeight - y(d.frequency));
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", d => x(d.letter))
+      .attr("y", d => y(d.frequency))
+      .attr("width", x.bandwidth())
+      .attr("height", d => contentHeight - y(d.frequency));
   }
 
   onResize() {
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,52 +1,59 @@
-import { Globals } from './../utils/Globals';
-import { User } from './../user/user.component';
-import { Component, OnInit } from '@angular/core';
-import UserService from '../../services/user.service';
-import { UserModel } from '../../models/user';
-import {Router, RouterModule} from '@angular/router';
-import {CustomerService} from '../utils/CustomerService';
+import { Globals } from "./../utils/Globals";
+import { User } from "./../user/user.component";
+import { Component, OnInit } from "@angular/core";
+import UserService from "../../services/user.service";
+import { UserModel } from "../../models/user";
+import { Router, RouterModule } from "@angular/router";
+import { CustomerService } from "../utils/CustomerService";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
-
 export class Login implements OnInit {
   private user: UserModel;
 
   private userLogin = {
     mail: "",
     password: ""
-  }; 
-  
+  };
+
   private loginMessage = "";
-  
-  constructor (private userServ: UserService, 
-               private customer: CustomerService, 
-               private router: Router,
-               private globals: Globals) {
-    }
 
-    ngOnInit (){
-    }
+  constructor(
+    private userServ: UserService,
+    private customer: CustomerService,
+    private router: Router,
+    private globals: Globals
+  ) {}
 
-    login() {
-      this.userServ.getUserByMailAndPassword(this.userLogin.mail, this.userLogin.password).subscribe(
-        res => { 
+  ngOnInit() {
+    var canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "30px Comic Sans MS";
+    ctx.fillStyle = "red";
+    ctx.textAlign = "center";
+    ctx.fillText("Hello giver", canvas.width / 2, canvas.height / 2);
+  }
+
+  login() {
+    this.userServ
+      .getUserByMailAndPassword(this.userLogin.mail, this.userLogin.password)
+      .subscribe(
+        res => {
           if (!res.user) {
             this.loginMessage = "Username or Password are incorrect!";
-          }
-          else {
+          } else {
             this.user = res.user;
             this.globals.connectedUser = this.user;
             this.customer.setToken(JSON.stringify(res.user));
-            this.router.navigate(['/home/my-activity']);
-          }   
-         },
-         err => {
-          this.loginMessage = "Connection failed!"; 
-         }
+            this.router.navigate(["/home/my-activity"]);
+          }
+        },
+        err => {
+          this.loginMessage = "Connection failed!";
+        }
       );
-    }
+  }
 }
