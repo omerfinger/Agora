c3d5d66fc976b60e11a5e42e8796dc20f13e64f2
omerfinger <omerfinger@gmail.com>
2019-07-30 20:03:34 +0300

    design fix and craigslist UI

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to LostAndFound!');
+    expect(page.getTitleText()).toEqual('Welcome to Give And Take!');
   });
 
   afterEach(async () => {
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,26 +1,29 @@
-import { MessagesPipe } from './../../models/messagesPipe.pipe';
-import { BarChartComponent } from './../bar-chart/bar-chart.component';
-import { PieChart } from './../pie-chart/pie-chart.component';
-import { NgModule } from '@angular/core';
-import { NavbarMenu } from '../navber/navbar.component';
-import { MyActivity } from '../my-activity/my-activity.component';
-import { Lost } from '../lost/lost.component';
-import { Found } from '../found/found.component';
-import { Admin, RemoveUserDialogContent } from '../admin/admin.component';
-import { Stats } from '../stats/stats.component';
-import { User, EditUserDialogContent } from '../user/user.component';
-import { RouterModule, Routes } from '@angular/router';
-import { Home } from './home.component';
-import { ListItemComponent } from '../list-item/list-item.component';
-import { SearchItemsComponent } from '../search-items/search-items.component';
-import { SearchUsersComponent } from './../search-users/search-users.component';
-import { CreateItemComponent, CreateItemDialogComponent } from './../create-item/create-item.component';
-import { UpdateItemComponent } from './../update-item/update-item.component';
-import { ListMessagesComponent } from './../list-messages/list-messages.component'
+import { MessagesPipe } from "./../../models/messagesPipe.pipe";
+import { BarChartComponent } from "./../bar-chart/bar-chart.component";
+import { PieChart } from "./../pie-chart/pie-chart.component";
+import { NgModule } from "@angular/core";
+import { NavbarMenu } from "../navber/navbar.component";
+import { MyActivity } from "../my-activity/my-activity.component";
+import { Lost } from "../lost/lost.component";
+import { Found } from "../found/found.component";
+import { Admin, RemoveUserDialogContent } from "../admin/admin.component";
+import { Stats } from "../stats/stats.component";
+import { User, EditUserDialogContent } from "../user/user.component";
+import { RouterModule, Routes } from "@angular/router";
+import { Home } from "./home.component";
+import { ListItemComponent } from "../list-item/list-item.component";
+import { SearchItemsComponent } from "../search-items/search-items.component";
+import { SearchUsersComponent } from "./../search-users/search-users.component";
+import {
+  CreateItemComponent,
+  CreateItemDialogComponent
+} from "./../create-item/create-item.component";
+import { UpdateItemComponent } from "./../update-item/update-item.component";
+import { ListMessagesComponent } from "./../list-messages/list-messages.component";
 import { GooglePlaceModule } from "ngx-google-places-autocomplete";
+import { CraigsListItem } from "../craigslisitem/craigslistitem.component";
 
-import UserService from '../../services/user.service';
-
+import UserService from "../../services/user.service";
 
 import {
   MatSidenavModule,
@@ -41,17 +44,17 @@ import {
   MatFormFieldModule,
   MatTooltipModule,
   MatChipsModule
-} from '@angular/material';
+} from "@angular/material";
 
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpModule } from '@angular/http';
-import { AgmCoreModule } from '@agm/core';
-import { CreateMessageDialogComponent } from '../create-message/create-message-dialog.component';
-import { NewMessageDialogComponent } from '../new-message-dialog/new-message-dialog.component';
-import { MyMessagesComponent } from '../my-messages/my-messages.component';
-import { MyItemsComponent } from '../my-items/my-items.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpModule } from "@angular/http";
+import { AgmCoreModule } from "@agm/core";
+import { CreateMessageDialogComponent } from "../create-message/create-message-dialog.component";
+import { NewMessageDialogComponent } from "../new-message-dialog/new-message-dialog.component";
+import { MyMessagesComponent } from "../my-messages/my-messages.component";
+import { MyItemsComponent } from "../my-items/my-items.component";
 
 @NgModule({
   imports: [
@@ -81,11 +84,10 @@ import { MyItemsComponent } from '../my-items/my-items.component';
     MatTooltipModule,
     MatChipsModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDe2cx9NLqtDipMKZ1J2EeioMAn2W9L_20'
+      apiKey: "AIzaSyDe2cx9NLqtDipMKZ1J2EeioMAn2W9L_20"
     })
   ],
-  exports: [
-  ],
+  exports: [],
   declarations: [
     NavbarMenu,
     MyActivity,
@@ -93,14 +95,15 @@ import { MyItemsComponent } from '../my-items/my-items.component';
     Found,
     Admin,
     Stats,
-    User, 
+    User,
     EditUserDialogContent,
     RemoveUserDialogContent,
-    Home, 
+    Home,
     CreateItemComponent,
     CreateItemDialogComponent,
     UpdateItemComponent,
     ListItemComponent,
+    CraigsListItem,
     SearchItemsComponent,
     SearchUsersComponent,
     PieChart,
@@ -119,11 +122,11 @@ import { MyItemsComponent } from '../my-items/my-items.component';
     CreateItemComponent,
     CreateItemDialogComponent,
     UpdateItemComponent,
-    ListItemComponent,
+    CraigsListItem,
     CreateMessageDialogComponent,
     NewMessageDialogComponent,
     Home
   ],
   bootstrap: [Home]
 })
-export class HomeModule { }
+export class HomeModule {}
